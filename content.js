4b6e62121aeecfddcc644ff3e4c12c7a1bdea11c
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-05-28 11:17:08 +0530

    fixed some issues

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ console.log("started..........")
 const newProfilePicURL = 'https://i.postimg.cc/FsvMh1s2/pfp.jpg';
 
 function replaceProfilePictures() {
-  const profilePics = document.querySelectorAll(' img.EntityPhoto-circle-0, img.EntityPhoto-circle-3');
+  const profilePics = document.querySelectorAll(' img.EntityPhoto-circle-0, img.EntityPhoto-circle-3, img.EntityPhoto-square-3, img.ivm-image-view-model__square-img, img.ivm-image-view-model__circle-img, img.EntityPhoto-square-3, img.EntityPhoto-circle-5');
   profilePics.forEach(pic => {
     pic.src = newProfilePicURL;
   });
